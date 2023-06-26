08005d656b41c61727d264d23bb7aeb1638e97c8
PdoyleC <>
2023-06-26 13:14:52 +0000

    linked quizover htmal to index html

--- assets/js/quizover.js
+++ assets/js/quizover.js
@@ -2,6 +2,7 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const RecentScore = localStorage.getItem("RecentScore");
+const restartBut = document.getElementById('restart-btn');
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const MAX_HIGH_SCORES = 5;
 
@@ -26,3 +27,23 @@ saveHighScore = (e) => {
     localStorage.setItem("highScores", JSON.stringify(highScores));
     window.location.assign("highscores.html");
 };
+restartBut.onclick = function () {
+    restartBut.classList.add('hide');
+    questionCont.classList.add('hide');
+    letsGo.classList.remove('hide');
+    currentCorrectScore = 0;
+    currentIncorrectScore = 0;
+    sec = 60;
+    document.getElementById('correct').innerText = currentCorrectScore;
+    document.getElementById('incorrect').innerText = currentIncorrectScore;
+    return window.location.assign("index.html");
+    runGame();
+};
+
+function runGame() {
+    letsGo.classList.add('hide');
+    shuffledQuestions = questions.sort(() => .5 - Math.random()).slice(0, 10);
+    currentQuestionIndex = 0;
+    questionCont.classList.remove('hide');
+    getNextQuestion();
+}
